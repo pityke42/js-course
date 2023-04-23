7f62116062335d57241f15e2a7ac3474100fe0d7
István Balla <ballllaistvan@gmail.com>
2023-04-23 15:11:47 +0200

    modal things

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,2 +1,22 @@
 'use strict';
-répa
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+console.log(btnsOpenModal)
+
+for(let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+    btnCloseModal.addEventListener('click', function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+    overlay.addEventListener('click', function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    })
+}
+
