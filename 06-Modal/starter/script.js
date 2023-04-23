77a12740b43b37c7cfbca3d8d7fd20fad1c4629d
István Balla <ballllaistvan@gmail.com>
2023-04-23 16:25:42 +0200

    modal hid by pressing ecs

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,18 +5,27 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 console.log(btnsOpenModal)
 
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const keydown = function(event){
+        if(event.key === 'Escape'){
+            if(!modal.classList.contains('hidden')){
+                closeModal()
+            }
+        }
+}
 for(let i = 0; i < btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('click', function(){
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
-    btnCloseModal.addEventListener('click', function(){
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    });
-    overlay.addEventListener('click', function(){
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    })
+    btnsOpenModal[i].addEventListener('click', openModal);
+  
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
+    document.addEventListener('keydown', keydown)
 }
 
