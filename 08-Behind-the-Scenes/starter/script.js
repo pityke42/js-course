7059df550a82e1cefffdfd2f2d4c312275c5068b
István Balla <ballllaistvan@gmail.com>
2023-04-26 11:31:30 +0200

    hoisting

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear){
     const age = 2023 - birthYear;
     
@@ -32,3 +32,46 @@ function calcAge(birthYear){
 
 const firstName = 'Pityke';
 calcAge(1998);
+
+
+//hoisiting variables
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Pityke';
+let job = 'jobless';
+const year = 1998;
+
+//hoiseting functions
+console.log(addArrow(2, 3));
+console.log(addDecl(2, 3));
+console.log(addExp(2, 3));
+
+function addDecl(a, b){
+    return a + b;
+}
+const addExp = function(a, b){
+    return a + b;
+}
+const addArrow = (a, b) => {
+    a + b;
+}
+
+//example
+if(!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart(){
+    console.log('all produccts deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x)
+*/
+
+
+
