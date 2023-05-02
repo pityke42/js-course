262e83385aea11802f9ef8eabcdfc27dab8a5e25
István Balla <ballllaistvan@gmail.com>
2023-05-02 10:35:32 +0200

    for of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -349,4 +349,58 @@ console.log(main, secondary);
 //  team1 < team2 && console.log('team 1 is more likely to win');
 //  team1 > team2 && console.log('team 2 is more likely to win');
 
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `order received ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered ${address} at ${time}`
+    );
+  },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  orederPasta: function (ing1, ing2, ing3) {
+    console.log(`here is your delicious pasta ${ing1}, ${ing2} and ${ing3}`);
+  },
+  orderPizza: function(mainIngrediend, ...otherIngredient){
+    console.log(mainIngrediend);
+    console.log(otherIngredient);
+  }
+};
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
+// for (const item of menu){
+//   console.log(item);
+// }
+// for (const item of menu.entries()){
+//   console.log(item);
+// }
+// console.log(menu.entries());
+// console.log([...menu.entries()]);
+
+// for (const [i, el] of menu.entries()){
+//   console.log(`${i + 1} : ${el}`);
+// }
