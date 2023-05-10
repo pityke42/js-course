7c9bf68e818ffe10dc27fbaf603dc963d1e022b3
István Balla <ballllaistvan@gmail.com>
2023-05-10 15:32:52 +0200

    forEach on maps and sets

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,102 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+/*
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//Slice(returns a new array not mutate the original) //1.index starts extracting//2.index is the end parameter
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -1));
+ //create a shallow copy with slice
+ console.log([...arr]);
+ 
+//Splice //mutates the original array
+//console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//Reverse //muatates the original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//Concat //does NOT mutate the original array
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//Join
+console.log(letters.join(' - '));
+
+//Push
+//Unshift
+//Shift
+//Indexof
+//Includes
+
+//At
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//getting the last element 
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1)); //-1 gets the last element of an array
+console.log('pityke'.at(0));
+console.log('pityke'.at(-1));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+ for (const [i, movement] of movements.entries()){
+  if(movement > 0){
+    console.log(`Movement ${i + 1}: You deposited ${movement}`)
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+  }
+ };
+
+ movements.forEach(function(movement, index, array){
+  if(movement > 0){
+    console.log(`Movement ${index + 1}: You deposited ${movement}`)
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`)
+  }
+ });
+ 
+ //0: function(200);
+ //1: function(450);
+ //2: function(-400);
+ */
+
+ //MAP
+ const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function(value, key, map){
+  console.log(`${key}: ${value} `);
+});
 
-/////////////////////////////////////////////////
+//SET
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function(value, _, set){
+  console.log(`${_}: ${value}`);
+})
---
+++ 11-Arrays-Bankist/starter/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+ for (const movement of movements){
+//   if(movement > 0){
+//     console.log(`You deposited ${movement}`)
+//   } else {
+//     console.log(`You withdrew ${Math.abs(movement)}`)
+//   }
+//  };
