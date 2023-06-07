59d3ab0442c617019e920e5e2a4ef2541770b675
pityke42 <ballllaistvan@gmail.com>
2023-06-07 11:30:06 +0200

    constructor function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const Person = function(firstName, birthYear){
+    //Instance Properies
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+}
+const jonas = new Person('Jonas', 1991); //instance from a person
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
