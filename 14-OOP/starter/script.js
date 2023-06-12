cbaa4ecd27bbb88bbbc2682c03b889d26fabd0cb
pityke42 <ballllaistvan@gmail.com>
2023-06-12 11:42:13 +0200

    coding challange 3.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -249,7 +249,6 @@ console.log(ford.break());
 console.log(ford.speedUS);
 ford.speedUS = 50;
 console.log(ford);
-*/
 
 //Inheritance between Classes and constructor functions
 const Person = function(firstName, birthYear){
@@ -288,3 +287,41 @@ console.log(mike instanceof Student);
 console.log(mike instanceof Person);
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+//Coding challange 3.
+const Car = function(make, speed){
+      this.make = make;
+      this.speed = speed;
+};
+const EV = function(make, speed, charge){
+   Car.call(this, make, speed);
+   this.charge = charge;
+};
+Car.prototype.accelerate = function(speed){
+   console.log()
+};
+Car.prototype.break = function(){
+   this.speed -= 5;
+   return this.speed;
+};
+
+
+
+//Link the prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function(chargeTo){
+   this.charge = chargeTo;
+};
+EV.prototype.accelerate= function(){
+   this.speed += 20;
+   this.charge--;
+   console.log(`${this.make} is going at ${this.speed} km/h, and charge of ${this.charge}`)
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+tesla.break();
+
+console.log(tesla);
+tesla.accelerate();
